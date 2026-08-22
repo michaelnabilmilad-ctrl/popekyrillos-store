8fa3283cc126864d623023a1e13cd0d38b557e38
Michael Nabil <michaelnabilmilad@gmail.com>
2026-08-22 21:48:00 +0300

    Bump taxonomy version for restored images

--- dist/category-taxonomy.js
+++ dist/category-taxonomy.js
@@ -889,7 +889,7 @@
   ];
   const TAXONOMY_STORAGE_KEY = "pope-kyrillos-taxonomy";
   const TAXONOMY_VERSION_STORAGE_KEY = "pope-kyrillos-taxonomy-version";
-  const CURRENT_TAXONOMY_VERSION = 1787412198099;
+  const CURRENT_TAXONOMY_VERSION = 1787424461727;
   const categories = (() => {
     try {
       const storedVersion = Number(localStorage.getItem(TAXONOMY_VERSION_STORAGE_KEY) || 0);
